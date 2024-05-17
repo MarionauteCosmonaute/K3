ef6cfc309e6351c43825493d14f9980c254852c0
Wicklyy <ryanro2001@live.fr>
2024-05-17 11:57:44 +0200

    historique

--- src/Model/Historique.java
+++ src/Model/Historique.java
@@ -7,21 +7,24 @@ import java.awt.Point;
 public class Historique{
     public class Coup{
         int type;
-        Point[] coordonnee; 
+        Point[] coordonnee;
         
-        Coup(int type){
+        Coup(int type, Point[] coordonnee){
             this.type = type;
+            this.coordonnee = coordonnee;
         }
-    
     }
 
     Stack<Coup> coup_jouer;
-    Stack<Coup> coup_annuler;
+    Stack<Coup> coup_retablie;
 
     public Historique(){
         coup_jouer = new Stack<>();
-        coup_annuler = new Stack<>();
+        coup_retablie = new Stack<>();
+    }
 
+    public void action(int type, Point[] coordonee){
+        coup_jouer.add(new Coup(type, coordonee));
     }
 
     
