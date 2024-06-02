04168c47fcc55c101d53a8256c0f5290e78b2cb6
Wicklyy <ryanro2001@live.fr>
2024-06-02 17:33:07 +0200

    regler des petit soucis

--- src/Reseau/Runnables/Transfer/Productor.java
+++ src/Reseau/Runnables/Transfer/Productor.java
@@ -10,31 +10,28 @@ import Model.Coup;
 public class Productor implements Runnable{
     BufferedReader in;
     ArrayList<Fifo> file;
-    Fifo coupJouer;
     int index;
 
-    public Productor(BufferedReader in, ArrayList<Fifo> file,Fifo coupJouer, int index){
-        init(in,file,coupJouer);
+    public Productor(BufferedReader in, ArrayList<Fifo> file, int index){
+        init(in,file);
         this.index = index;
     }
     public Productor(BufferedReader in, ArrayList<Fifo> file){
-        init(in,file,coupJouer);
         index = -1;
     }
 
-    public void init(BufferedReader in, ArrayList<Fifo> file,Fifo coupJouer){
+    public void init(BufferedReader in, ArrayList<Fifo> file){
         this.in = in;
         this.file = file;
-        this.coupJouer = coupJouer;
     }
 
 
     @Override
     public void run(){
-        //String[] split;
+        
         try{
             while (true) {
-                Coup coup = coupJouer.get();
+                Coup coup = Coup.fromString(in.readLine());
                 for(int i = 0; i < file.size(); i++){
                     if(i != index){
                         file.get(i).add(coup);
